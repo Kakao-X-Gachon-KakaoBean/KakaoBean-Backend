eead15c8bbfb0aee97ce8d3255e90bed9fb40fef
happysubin <asb0711@gmail.com>
2023-04-06 11:04:39 +0900

    feat: jacoco 적용

--- api/src/main/java/com/kakaobean/KakaoBeanApplication.java
+++ api/src/main/java/com/kakaobean/KakaoBeanApplication.java
@@ -1,17 +1,11 @@
 package com.kakaobean;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.annotation.PostConstruct;
-import java.util.List;
-
 @EnableJpaAuditing
-
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class KakaoBeanApplication {

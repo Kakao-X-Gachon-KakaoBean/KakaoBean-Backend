81ce4d8f5d3cfa5483a172269ee5a5fc9c6ce94f
happysubin <asb0711@gmail.com>
2023-06-02 23:10:28 +0900

    feat: 설문, 응답 api 로그 기능 추가

--- api/src/main/java/com/kakaobean/response/ResponseController.java
+++ api/src/main/java/com/kakaobean/response/ResponseController.java
@@ -8,12 +8,14 @@ import com.kakaobean.core.response.application.dto.response.RegisterSurveyRespon
 import com.kakaobean.response.dto.request.RegisterSurveyResponseRequest;
 import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Timed("api.response")
 @RestController
 @RequestMapping("/responses")
@@ -26,22 +28,27 @@ public class ResponseController {
     @PostMapping
     public ResponseEntity resisterSurveyResponse(@AuthenticationPrincipal Long memberId,
                                                  @RequestBody @Validated RegisterSurveyResponseRequest request){
+        log.info("설문 응답 생성 api 호출");
         RegisterSurveyResponseSubmmitDto res = responseService.registerSurveyResponse(request.toServiceDto(memberId));
+        log.info("설문 응답 생성 api 종료");
         return new ResponseEntity(res, HttpStatus.OK);
     }
 
     @GetMapping("/{surveyId}")
     public ResponseEntity finResponses(@AuthenticationPrincipal Long memberId,
                                        @PathVariable Long surveyId){
+        log.info("응답 조회 api 호출");
         FindResponsesDto res = responseProvider.findResponses(memberId, surveyId);
+        log.info("응답 조회 api 종료");
         return new ResponseEntity(res, HttpStatus.OK);
     }
 
     @GetMapping("survey-statistics/{surveyId}")
     public ResponseEntity findSurveyStatistics(@AuthenticationPrincipal Long memberId,
                                                @PathVariable Long surveyId){
+        log.info("응답 통계 조회 api 호출");
         FindSurveyStatisticsResponseDto res = responseProvider.findSurveyStatistics(memberId,surveyId);
+        log.info("응답 통계 조회 api 종료");
         return new ResponseEntity(res, HttpStatus.OK);
     }
-
 }
--- api/src/main/java/com/kakaobean/survey/SurveyController.java
+++ api/src/main/java/com/kakaobean/survey/SurveyController.java
@@ -7,12 +7,14 @@ import com.kakaobean.core.survey.application.dto.response.*;
 import com.kakaobean.survey.dto.request.RegisterSurveyRequest;
 import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Timed("api.survey")
 @RestController
 @RequestMapping("/surveys")
@@ -25,39 +27,51 @@ public class SurveyController {
     @PostMapping
     public ResponseEntity registerSurvey(@AuthenticationPrincipal Long memberId,
                                          @RequestBody @Validated RegisterSurveyRequest request) {
+        log.info("설문 생성 api 호출");
         RegisterSurveyResponseDto res = surveyService.registerSurvey(request.toServiceDto(memberId));
+        log.info("설문 생성 api 종료");
         return new ResponseEntity(res, HttpStatus.OK);
     }
 
     @GetMapping("/{surveyId}")
     public ResponseEntity findSurvey(@PathVariable Long surveyId) {
+        log.info("설문 조회 api 호출");
         FindSurveyResponseDto res = surveyProvider.getSurvey(surveyId);
+        log.info("설문 조회 api 종료");
         return new ResponseEntity(res, HttpStatus.OK);
     }
 
     @GetMapping("/own-survey")
     public ResponseEntity findOwnSurvey(@AuthenticationPrincipal Long memberId) {
+        log.info("본인이 만든 설문 조회 api 호출");
         FindOwnSurveyListResponseDto res = surveyProvider.getOwnSurvey(memberId);
+        log.info("본인이 만든 설문 조회 api 종료");
         return new ResponseEntity(res, HttpStatus.OK);
     }
 
     @GetMapping("/submitted-survey")
     public ResponseEntity findSubmittedSurvey(@AuthenticationPrincipal Long memberId){
+        log.info("본인이 참여한 설문 조회 api 호출");
         FindSubmittedSurveyListResponseDto res = surveyProvider.getSubmittedSurvey(memberId);
+        log.info("본인이 참여한 설문 조회 api 종료");
         return new ResponseEntity(res, HttpStatus.OK);
     }
 
     @DeleteMapping("/{surveyId}")
     public ResponseEntity removeSurvey(@AuthenticationPrincipal Long memberId,
                                        @PathVariable Long surveyId){
+        log.info("설문 삭제 api 호출");
         surveyService.removeSurvey(memberId, surveyId);
+        log.info("설문 삭제 api 종료");
         return new ResponseEntity(new CommandSuccessResponse(), HttpStatus.OK);
     }
 
     @PatchMapping("/{surveyId}")
     public ResponseEntity closeSurvey(@AuthenticationPrincipal Long memberId,
                                       @PathVariable Long surveyId){
+        log.info("설문 마감 api 호출");
         surveyService.closeSurvey(memberId, surveyId);
+        log.info("설문 마감 api 종료");
         return new ResponseEntity(new CommandSuccessResponse(), HttpStatus.OK);
     }
 

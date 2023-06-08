aa400d2a76cc90653667227df51be6f0a8a3938f
happysubin <asb0711@gmail.com>
2023-06-08 18:00:13 +0900

    fix: 주석 제거

--- independently-system/src/main/java/com/kakaobean/independentlysystem/email/dto/SesServiceRequest.java
+++ independently-system/src/main/java/com/kakaobean/independentlysystem/email/dto/SesServiceRequest.java
@@ -22,16 +22,12 @@ public class SesServiceRequest {
         this.content = content;
     }
 
-    public void addTo(String email){
-        this.to.add(email);
-    }
-
     public SendEmailRequest toSendRequestDto(){
         Destination destination = new Destination().withToAddresses(this.to);
 
         Message message = new Message()
                 .withSubject(createContent(this.subject))
-                .withBody(new Body().withHtml(createContent(this.content))); // content body는 HTML 형식으로 보내기 때문에 withHtml을 사용합니다.
+                .withBody(new Body().withHtml(createContent(this.content)));
 
         return new SendEmailRequest()
                 .withSource(this.from)
